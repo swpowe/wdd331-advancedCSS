0dd6a5e0e294e77ca99d62359b65bdba0d308216
Spencer Powell <spencer@alegralearning.com>
2021-11-17 17:16:45 -0700

    added week 11 sliding menu

---
+++ Week 11/sliding-menu/script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    // var updateElement = document.getElementById("menu-icon");
+    var updateElement = document.getElementById("sidebar");
+ //toggle adds a class if it's not there or removes it if it is.
+ updateElement.classList.toggle("open");
+//  alert("clicked");
+}   
