cf321036904dc858c611c5c6a01eb6f441b7ee57
Spencer Powell <spencer@alegralearning.com>
2021-12-03 08:49:49 -0700

    upload of archive

---
+++ Week 12/ARCHIVE ONLY will not run from this dir/src/js/index.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+
+// check to see if javascript is working
+// function component() {
+//   const element = document.createElement("p");
+
+//   element.innerHTML = "Setup Appears to be working &#128521;";
+
+//   return element;
+// }
+// document.body.appendChild(component()); 
+
+// test jquery and Bootstrap setup
+// $(document).ready(() => {
+//   const element = $("<p></p>").html("Jquery Appears to be working &#128521;");
+
+//   element.appendTo("body");
+//   $(() => {
+//     $('[data-toggle="popover"]').popover();
+//   });
+// });
